58d9db52a8307f187bcede69cd8af768a6afd956
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-15 11:13:02 -0600

    Add required function, Add function implementation

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 
 fetchMyIP((error, ip) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It didn't work!", error);
     return;
   }
-  console.log('It worked! Returned IP:' , ip);
-  
+  console.log("Returned IP:", ip);
+
   fetchCoordsByIP(ip, (error, coordinates) => {
     if (error) {
-      console.log("It didn't work!" , error);
+      console.log("It didn't work!", error);
       return;
     }
-    console.log('It worked! Returned Coordinates:' , coordinates);
+    console.log("Returned Coordinates:", coordinates);
+
+    fetchISSFlyOverTimes(coordinates, (error, passTimes) => {
+      if (error) {
+        console.log("It didn't work!", error);
+        return;
+      }
+      console.log("Returned flyover Times:", passTimes);
+    });
   });
 });
-
