90218efd7c2ddd0aff8bed6516ef796517b61762
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-15 10:31:04 -0600

    Add function to iss.js to fetch coordinates using IP output of previous async function

--- iss.js
+++ iss.js
@@ -21,9 +21,31 @@ const fetchMyIP = function(callback) {
       return;
     } else {
       const ip = body.ip;
-      callback(error, ip);
+      callback(null, ip);
     }
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  needle.get(`https://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (!body.success) {
+      const msg = `Success status: ${body.success} when fetching coordinates. Server message: ${body.message}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      const coordinates = {
+        latitude: body.latitude.toString(),
+        longitude: body.longitude.toString(),
+      };
+
+      callback(null, coordinates);
+    }
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
