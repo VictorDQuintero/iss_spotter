faf1d69b9763201b548381ba437e74261a85826a
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-16 10:52:07 -0600

    Add code to do the same as index.js but with promises

--- index2.js
+++ index2.js
@@ -1,3 +1,14 @@
-const { fetchMyIP } = require("./iss_promised");
+const { nextISSTimesForMyLocation } = require("./iss_promised");
 
-fetchMyIP().then((ip) => console.log(ip));
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds`);
+  }
+};
+
+nextISSTimesForMyLocation().then((passtimes) => {
+  printPassTimes(passtimes);
+});
